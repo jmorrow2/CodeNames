9892b5fe54a4d380fc6c69676e05bcaf8760794a
jmorrow2 <36283240+jmorrow2@users.noreply.github.com>
2018-02-27 18:13:40 -0500

    Update Location.java

--- src/code/Location.java
+++ src/code/Location.java
@@ -1,5 +1,15 @@
 package code;
 
 public class Location {
+	
+	public String codename;
+	public String person;
+	public boolean revealed;
+	
+	public Location(String cd, String p) {
+		codename = cd;
+		person = p;
+		revealed = false;
+	}
 
 }
