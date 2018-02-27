7ac6b67b1f8c44c4e24efed20faf3785c8a94965
Harsh <Harsh@HPSpectre>
2018-02-27 18:11:12 -0500

    Test.txt

--- src/code/Board.java
+++ src/code/Board.java
@@ -1,5 +1,22 @@
 package code;
 
+//import java.awt.Color;
+//import java.awt.Graphics;
+
 public class Board {
 
+	/**public void ColorGrid(Graphics colorScheme) {
+        
+		for ( int i = 0;  i < 5;  i++ ) {
+			for ( int I = 0;  I < 5;  I++ ) {
+				if (i + I % 2 == 1) {
+					colorScheme.setColor(Color.BLUE);
+					}
+			
+			else if (i + I % 2 == 0) {
+				colorScheme.setColor(Color.RED);	
+			}
+				}
+			}
+		}*/
 }
