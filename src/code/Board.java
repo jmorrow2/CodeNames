385261c03c34a6c6726dc85c75a25572d4e6786f
Harsh Patel <32227637+harsh104@users.noreply.github.com>
2018-03-01 15:51:25 -0500

    Update Board.java

--- src/code/Board.java
+++ src/code/Board.java
@@ -10,3 +10,19 @@ public class Board {
 		locArray = l;
 	}
 }
+
+//THIS IS FOR GUI.
+/**public void ColorGrid(Graphics colorScheme) { 
+        
+		for ( int i = 0;  i < 5;  i++ ) {
+			for ( int I = 0;  I < 5;  I++ ) {
+				if (i + I % 2 == 1) {
+					colorScheme.setColor(Color.BLUE);
+					}
+			
+			else if (i + I % 2 == 0) {
+				colorScheme.setColor(Color.RED);	
+			}
+				}
+			}
+		}*/
