1e98cb1cbba50b8e03277b420c445d3472c02ca9
jmorrow2 <36283240+jmorrow2@users.noreply.github.com>
2018-03-01 15:49:19 -0500

    Update Board.java

--- src/code/Board.java
+++ src/code/Board.java
@@ -1,39 +1,12 @@
 package code;
 
-//import java.awt.Color;
-//import java.awt.Graphics;
+import java.util.ArrayList;
 
 public class Board {
-	private Model m;
 	
-	public Location[] locArray;
+	public ArrayList<Location> locArray;
 
-//		public Board() {
-//	for(int i =0; i<25; i++) {
-//		locArray[i] = new Location[i];     	// We have to make 25 Location instances
-											// and assign 2 strings and a boolean to each array
-											// still trying to figure that out. -victoria
-
-
-	
-	//this is just creating 1 instance of location with a length of 25??
-	public Board() {
-	locArray = new Location[25];
-}
-	
-
-	/**public void ColorGrid(Graphics colorScheme) {
-        
-		for ( int i = 0;  i < 5;  i++ ) {
-			for ( int I = 0;  I < 5;  I++ ) {
-				if (i + I % 2 == 1) {
-					colorScheme.setColor(Color.BLUE);
-					}
-			
-			else if (i + I % 2 == 0) {
-				colorScheme.setColor(Color.RED);	
-			}
-				}
-			}
-		}*/
+	public Board(ArrayList<Location> l) {
+		locArray = l;
+	}
 }
