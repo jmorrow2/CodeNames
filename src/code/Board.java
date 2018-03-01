464ec652cfe07b0a7837a39a49a142cabf3ba83c
jmorrow2 <36283240+jmorrow2@users.noreply.github.com>
2018-03-01 16:39:34 -0500

    Update Board.java

--- src/code/Board.java
+++ src/code/Board.java
@@ -1,38 +1,40 @@
 package code;
 
-/** This class creates a Board of 25 Location Instances. 
-*/
-
 import java.util.ArrayList;
 
 public class Board {
 	
-	/** Instance Varaiable of type ArrayList with Location as arguments. 
-	*/
+	/**An arrayList where the boards location instances are stored */
+	private ArrayList<Location> locArray;
 	
-	public ArrayList<Location> locArray;
-	
-	/** This method is the the constructor for the class.
-	@param locArray: an array of 25 instances of Location.
-	*/
-
+	/** Instance of the Board class is use to represent the game board 
+	 *@param ArrayList of location instances 
+	 **/
 	public Board(ArrayList<Location> l) {
 		locArray = l;
 	}
-}
+	
+	public ArrayList<Location> getLocArray() {
+		return locArray;
+	}
 
-//THIS IS FOR GUI.
-/**public void ColorGrid(Graphics colorScheme) { 
-        
-		for ( int i = 0;  i < 5;  i++ ) {
-			for ( int I = 0;  I < 5;  I++ ) {
-				if (i + I % 2 == 1) {
-					colorScheme.setColor(Color.BLUE);
-					}
-			
-			else if (i + I % 2 == 0) {
-				colorScheme.setColor(Color.RED);	
-			}
+	public void setLocArray(ArrayList<Location> locArray) {
+		this.locArray = locArray;
+	}
+
+	//THIS IS FOR GUI.
+	/**public void ColorGrid(Graphics colorScheme) { 
+	        
+			for ( int i = 0;  i < 5;  i++ ) {
+				for ( int I = 0;  I < 5;  I++ ) {
+					if (i + I % 2 == 1) {
+						colorScheme.setColor(Color.BLUE);
+						}
+				
+				else if (i + I % 2 == 0) {
+					colorScheme.setColor(Color.RED);	
 				}
-			}
-		}*/
+					}
+				}
+			}*/
+}
