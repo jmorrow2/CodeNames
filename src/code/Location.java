69895b580c6ac595dccf04fe66f3eaa77ff1102c
jmorrow2 <36283240+jmorrow2@users.noreply.github.com>
2018-03-01 16:09:14 -0500

    Update Location.java

--- src/code/Location.java
+++ src/code/Location.java
@@ -2,13 +2,21 @@ package code;
 
 public class Location {
 	
+	/** codename assigned to the location instance */
 	public String codename;
+	/** agent assigned to the location instance */
 	public String agent;
+	/** boolean declaring whether or not the locations agent has been revealed yet */
 	public boolean revealed;
 	
-	public Location(String cd, String p) {
+	/**Instances of this class are used to represent a space on the game board
+	 * 
+	 * @param cd codename to be assigned to  the location instance
+	 * @param a agent to be assigned to the location instance
+	 */
+	public Location(String cd, String a) {
 		codename = cd;
-		agent = p;
+		agent = a;
 		revealed = false;
 	}
 }
