3fa035d3a46941d0178d6859470bb2b176f540f6
jmorrow2 <36283240+jmorrow2@users.noreply.github.com>
2018-03-01 16:40:42 -0500

    Update codenamesTest.java

--- src/test/codenamesTest.java
+++ src/test/codenamesTest.java
@@ -24,13 +24,13 @@ public class codenamesTest {
 		ArrayList<Location> l = m.createLocationsArray();
 		Board b = m.createBoard(l);
 		assertNotNull("The createBoard method must create a board", b);
-		assertEquals("The board must have instances of Location equal to (m.row * m.column)", (m.row * m.column), b.locArray.size());
+		assertEquals("The board must have instances of Location equal to (m.row * m.column)", (m.getRow() * m.getColumn()), b.getLocArray().size());
 	}
 
 	@Test
 	public void readFileTest() {
 		Model m = new Model(5, 5, 9, 8, 7, 1, "src/GameWords.txt");
-		ArrayList<String> test = m.readFile(m.file);
+		ArrayList<String> test = m.readFile(m.getFile());
 		assertNotNull("The readFile method must create an ArrayList of the codenames", test);
 	}
 	
@@ -38,26 +38,26 @@ public class codenamesTest {
 	public void chooseCodenamesTest() {
 		Model m = new Model(5, 5, 9, 8, 7, 1, "src/GameWords.txt");
 		ArrayList<String> test = m.chooseCodenames();
-		assertEquals("The chooseCodenames method must create an ArrayList of codename strings equal to (m.row * m.column)", (m.row * m.column), test.size());
+		assertEquals("The chooseCodenames method must create an ArrayList of codename strings equal to (m.row * m.column)", (m.getRow() * m.getColumn()), test.size());
 		HashMap<String, Integer> hash = new HashMap<>();
-		for(int i=0; i<(m.row * m.column); i++) {
+		for(int i=0; i<(m.getRow() * m.getColumn()); i++) {
 			hash.put(test.get(i), 0);
 		}
-		assertEquals("The chosen codenames must all be unique", (m.row * m.column), hash.size());
+		assertEquals("The chosen codenames must all be unique", (m.getRow() * m.getColumn()), hash.size());
 	}
 	
 	@Test
 	public void createAgentsTest() {
 		Model m = new Model(5, 5, 9, 8, 7, 1, "src/GameWords.txt");
 		ArrayList<String> test = m.createAgents();
-		assertEquals("The createAgents method must create an ArrayList of agent strings equal to (m.row * m.column)", (m.row * m.column), (test.size()));
+		assertEquals("The createAgents method must create an ArrayList of agent strings equal to (m.getRow() * m.getColumn())", (m.getRow() * m.getColumn()), (test.size()));
 		
 		int redCount = 0;
 		int blueCount = 0;
 		int bystanderCount = 0;
 		int assassinCount = 0;
 		
-		for(int i=0; i<(m.row * m.column); i++) {
+		for(int i=0; i<(m.getRow() * m.getColumn()); i++) {
 			if (test.get(i).equals("Red")){
 				redCount++;
 			}
@@ -72,10 +72,10 @@ public class codenamesTest {
 			}
 		}
 		
-		assertEquals("The agentArray must contain red agents equal to the redAgents variable", m.redAgents, redCount);
-		assertEquals("The agentArray must contain blue agents equal to the blueAgents variable", m.blueAgents, blueCount);
-		assertEquals("The agentArray must contain bystanders agents equal to the bystanders variable", m.bystanders, bystanderCount);
-		assertEquals("The agentArray must contain assassins agents equal to the assassins variable", m.assassins, assassinCount);
+		assertEquals("The agentArray must contain red agents equal to the redAgents variable", m.getRedAgents(), redCount);
+		assertEquals("The agentArray must contain blue agents equal to the blueAgents variable", m.getBlueAgents(), blueCount);
+		assertEquals("The agentArray must contain bystanders agents equal to the bystanders variable", m.getBystanders(), bystanderCount);
+		assertEquals("The agentArray must contain assassins agents equal to the assassins variable", m.getAssassins(), assassinCount);
 		
 		ArrayList<String> test2 = m.createAgents();
 		assertNotEquals(test, test2);
@@ -84,13 +84,13 @@ public class codenamesTest {
 	@Test
 	public void createLocationsArrayTest() {
 		Model m = new Model(5, 5, 9, 8, 7, 1, "src/GameWords.txt");
-		assertTrue("When the game begins the redTurn variable must be true", m.redTurn);
-		assertFalse("When the game begins the blueTurn variable must be false", m.blueTurn);
+		assertTrue("When the game begins the redTurn variable must be true", m.getRedTurn());
+		assertFalse("When the game begins the blueTurn variable must be false", m.getBlueTurn());
 		Board b = m.createBoard(m.createLocationsArray());
-		for(Location l : b.locArray) {
-			assertFalse("Every Location instance must have a codename string", l.codename.isEmpty());
-			assertFalse("Every Location instance must have a agent string", l.agent.isEmpty());
-			assertFalse("Every Location instance must have a revealed boolean that is set to false", l.revealed);
+		for(Location l : b.getLocArray()) {
+			assertFalse("Every Location instance must have a codename string", l.getCodename().isEmpty());
+			assertFalse("Every Location instance must have a agent string", l.getAgent().isEmpty());
+			assertFalse("Every Location instance must have a revealed boolean that is set to false", l.getRevealed());
 		}
 	}
 	
@@ -98,17 +98,17 @@ public class codenamesTest {
 	public void createIlegalGuessArrayTest() {
 		Model m = new Model(5, 5, 9, 8, 7, 1, "src/GameWords.txt");
 		ArrayList<String> test = m.createIlegalGuessArray();
-		assertEquals("At the beginning of the game the illegalGuessArray must be the same as the codenamesArray", test, m.codenamesArray);
+		assertEquals("At the beginning of the game the illegalGuessArray must be the same as the codenamesArray", test, m.getCodenamesArray());
 	}
 	
 	@Test 
 	public void clueCheckTest() {
 		Model m = new Model(5, 5, 9, 8, 7, 1, "src/GameWords.txt");
-		String test = m.illegalGuessArray.get(0);
+		String test = m.getIllegalGuessArray().get(0);
 		assertFalse("The clueCheck method return false for a clue that is contained in the illegalGuessArray", m.clueCheck(test));
 		
 		String a = "--------";
-			for(String s : m.illegalGuessArray) {
+			for(String s : m.getIllegalGuessArray()) {
 				assertNotEquals(s, a);
 			}
 		assertTrue("The clueCheck method must return true for a clue that is not contained in the illegalGuessArray", m.clueCheck(a));
@@ -117,34 +117,34 @@ public class codenamesTest {
 	@Test
 	public void selectedTest() {
 		Model m = new Model(5, 5, 9, 8, 7, 1, "src/GameWords.txt");
-		m.count = 3;
-		assertTrue(m.redTurn);
-		assertFalse(m.blueTurn);
-		Location l = m.board.locArray.get(0);
-		assertFalse(l.revealed);
+		m.setCount(3);
+		assertTrue(m.getRedTurn());
+		assertFalse(m.getBlueTurn());
+		Location l = m.getBoard().getLocArray().get(0);
+		assertFalse(l.getRevealed());
 		
-		if(l.agent.equals("Red")) {
+		if(l.getAgent().equals("Red")) {
 			assertTrue("If a red agent is selected during the red teams turn the selected method must return true", m.selected(l));
 		}
 		
-		if(l.agent.equals("Blue") || l.agent.equals("Assassin") || l.agent.equals("Bystander")) {
+		if(l.getAgent().equals("Blue") || l.getAgent().equals("Assassin") || l.getAgent().equals("Bystander")) {
 			assertFalse("If an agent other than red is selected during the red teams turn the selected method must return false", m.selected(l));
 		}
 		
-		assertTrue("The selected method must change the selected Locations revealed boolean to true", l.revealed);
-		assertEquals("The selected method must decrement the count by one", 2, m.count);
+		assertTrue("The selected method must change the selected Locations revealed boolean to true", l.getRevealed());
+		assertEquals("The selected method must decrement the count by one", 2, m.getCount());
 		
-		m.redTurn = false;
-		m.blueTurn = true;
+		m.setRedTurn(false);
+		m.setBlueTurn(true);
 		
-		assertTrue(m.blueTurn);
-		assertFalse(m.redTurn);
+		assertTrue(m.getBlueTurn());
+		assertFalse(m.getRedTurn());
 		
-		if(l.agent.equals("Blue")) {
+		if(l.getAgent().equals("Blue")) {
 			assertTrue("If a blue agent is selected during the blue teams turn the selected method must return true", m.selected(l));
 		}
 		
-		if(l.agent.equals("Red") || l.agent.equals("Assassin") || l.agent.equals("Bystander")) {
+		if(l.getAgent().equals("Red") || l.getAgent().equals("Assassin") || l.getAgent().equals("Bystander")) {
 			assertFalse("If an agent other than blue is selected during the blue teams turn the selected method must return false", m.selected(l));
 		}
 	}
@@ -154,38 +154,38 @@ public class codenamesTest {
 		Model m = new Model(5, 5, 9, 8, 7, 1, "src/GameWords.txt");
 		assertFalse("The winningState method must return false when the game first begins and no agents are revealed", m.winningState());
 		
-		for(Location l : m.board.locArray) {
-			if(l.agent.equals("Blue")) {
-				l.revealed = true;
+		for(Location l : m.getBoard().getLocArray()) {
+			if(l.getAgent().equals("Blue")) {
+				l.setRevealed(true);
 			}
 		}
 		assertTrue("The winningState method must return true when all blue agents are revealed", m.winningState());	
 		
 		Model m2 = new Model(5, 5, 9, 8, 7, 1, "src/GameWords.txt");
-		for(Location l : m2.board.locArray) {
-			if(l.agent.equals("Red")) {
-				l.revealed = true;
+		for(Location l : m2.getBoard().getLocArray()) {
+			if(l.getAgent().equals("Red")) {
+				l.setRevealed(true);
 			}
 		}
 		assertTrue("The winningState method must return true when all red agents are revealed", m2.winningState());	
 		
 		Model m3 = new Model(5, 5, 9, 8, 7, 1, "src/GameWords.txt");
-		for(Location l : m3.board.locArray) {
-			if(l.agent.equals("Assassin")) {
-				l.revealed = true;
+		for(Location l : m3.getBoard().getLocArray()) {
+			if(l.getAgent().equals("Assassin")) {
+				l.setRevealed(true);
 			}
 		}
 		assertTrue("The winningState method must return true when all assassins are revealed", m3.winningState());	
 		
 		Model m4 = new Model(5, 5, 9, 8, 7, 1, "src/GameWords.txt");
-		for(Location l : m4.board.locArray) {
-			if(l.agent.equals("Red")) {
-				l.revealed = true;
+		for(Location l : m4.getBoard().getLocArray()) {
+			if(l.getAgent().equals("Red")) {
+				l.setRevealed(true);
 				break;
 			}
-			for(Location k : m4.board.locArray) {
-				if(k.agent.equals("Blue")) {
-					k.revealed = true;
+			for(Location k : m4.getBoard().getLocArray()) {
+				if(k.getAgent().equals("Blue")) {
+					k.setRevealed(true);
 					break;
 				}
 			}
@@ -196,13 +196,13 @@ public class codenamesTest {
 	@Test
 	public void assassinRevealedTest() {
 		Model m = new Model(5, 5, 9, 8, 7, 1, "src/GameWords.txt");
-		assertTrue(m.redTurn);
-		assertFalse(m.blueTurn);
+		assertTrue(m.getRedTurn());
+		assertFalse(m.getBlueTurn());
 		assertEquals("The assassinRevealed method must return Blue if the assassin is revealed during the Red turn", m.assassinRevealed(), "Blue");
-		m.redTurn = false;
-		m.blueTurn = true;
-		assertTrue(m.blueTurn);
-		assertFalse(m.redTurn);
+		m.setRedTurn(false);
+		m.setBlueTurn(true);
+		assertTrue(m.getBlueTurn());
+		assertFalse(m.getRedTurn());
 		assertEquals("The assassinRevealed method must return Red if the assassin is revealed during the Blue turn", m.assassinRevealed(), "Red");
 	}
 }
