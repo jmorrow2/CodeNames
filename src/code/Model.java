9e2e8e15d6ccf8e9e51ace6f4ac726d9f268d7fd
jmorrow2 <36283240+jmorrow2@users.noreply.github.com>
2018-03-01 15:48:52 -0500

    Update Model.java

--- src/code/Model.java
+++ src/code/Model.java
@@ -5,52 +5,56 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 
 public class Model {
 	
+	public Board board;
+	public int row;
+	public int column;
+	public int redAgents;
+	public int blueAgents;
+	public int bystanders;
+	public int assassins;
+	public String file;
 	public ArrayList<String> allCodenamesArray;
 	public ArrayList<String> codenamesArray;
 	public ArrayList<String> agentArray;
-	public ArrayList<String> redArray;
-	public ArrayList<String> blueArray;
-	public ArrayList<String> bystanderArray;
-	public ArrayList<String> assassinArray;
-	private Location[] l;
-	private ArrayList<String>the25names;
-
-
+	public ArrayList<Location> locArray;
+	public ArrayList<String> illegalGuessArray;
+	public boolean redTurn;
+	public boolean blueTurn;
+	public String clue;
+	public int count;
 	
-	/**
-	 * allCodenamesArray holds all possible code names that are read from a GameWords.txt 
-	 * the25names should hold the 25 different codenames that will be assigned to a location.
-	 * the agentArray holds the 9Red agents/8Blue agents/7Bystanders/1Assassin
-	 */
-	
-	public Model() {
-		Board b = new Board();
-		b.locArray = createLocationsArray();  //causes tests to fail?
-		allCodenamesArray = readFile();
-		the25names = chooseCodenames();
-		agentArray = createAgent();
+	public Model(int r, int c, int red, int blue, int bys, int assassin, String f) {
+		row = r;
+		column = c;
+		redAgents = red;
+		blueAgents = blue;
+		bystanders = bys;
+		assassins = assassin;
+		file = f;
+		allCodenamesArray = readFile(file);
+		codenamesArray = chooseCodenames();
+		illegalGuessArray = createIlegalGuessArray();
+		agentArray = createAgents();
+		locArray = createLocationsArray();
+		board = createBoard(locArray);
+		redTurn = true;
+		blueTurn = false;
 	}
 	
-	public void startNewGame() {
-		Board b = new Board();
-		//red teams move
-		//each boards 25 location is assigned a code name, person and not revealed
-		l = createLocationsArray();
-		for(int i = 0; i<25; i++) {
-		}
-		
+	public Board createBoard(ArrayList<Location> l) {
+		Board answer = new Board(l);
+		return answer;
 	}
 	
-	
-	public ArrayList<String> readFile() {
+	public ArrayList<String> readFile(String s) {
 		ArrayList<String> answer = new ArrayList<String>();
 		try {
-			String filename = "src/code/GameWords.txt";
+			String filename = s;
 	        for(String line : Files.readAllLines(Paths.get(filename))){
-	        //	allCodenamesArray.add(line);
 	        	answer.add(line);
 	        	}
 			} catch (IOException ex){
@@ -60,93 +64,107 @@ public class Model {
 	}
 	
 	public ArrayList<String> chooseCodenames() {
-		the25names = new ArrayList<String>();
+		HashMap<String, Integer> hash = new HashMap<String, Integer>();
+		ArrayList<String> answer = new ArrayList<String>();
 		Collections.shuffle(allCodenamesArray);
-		for(int i=0; i<25; i++) {
-			the25names.add(allCodenamesArray.get(i));
-			allCodenamesArray.remove(i);
+			for(String s : allCodenamesArray) {
+				hash.put(s, 0);
+				if(hash.size() == (row*column)) {
+					break;
+			}
 		}
-		return the25names;
+		for(String k : hash.keySet()) {
+			answer.add(k);
+		}
+		return answer;
 	}
 
-	public ArrayList<String> createAgent(){
+	public ArrayList<String> createAgents(){
 		ArrayList<String> answer = new ArrayList<String>();
-		for (int i=0; i<25; i++) {
-			if(i <10) {
-				answer.add("Red");
-				redArray.add("Red");
-			}	
-			if(i >=10 && i <18) {
-				answer.add("Blue");
-				blueArray.add("Blue");
+		for (int i=0; i<redAgents; i++) {
+			answer.add("Red");
 		}
-			if(i >=18 && i <24) {
-				answer.add("Bystander");
-				bystanderArray.add("Bystander");
+		for (int i=0; i<blueAgents; i++) {
+			answer.add("Blue");
 		}
-			if(i==24) {
-				answer.add("Assassin");
-				assassinArray.add("Assassin");
+		for (int i=0; i<bystanders; i++) {
+			answer.add("Bystander");
 		}
+		for (int i=0; i<assassins; i++) {
+		answer.add("Assassin");
 		}
 		Collections.shuffle(answer);
 		return answer;
 	}
 	
-	public Location[] createLocationsArray() {
-		l = new Location[25];
-		for(int i=0; i<25; i++) {
+	public ArrayList<Location> createLocationsArray() {
+		ArrayList<Location> answer = new ArrayList<Location>();
+		for(int i=0; i<(row*column); i++) {
 			Location loc = new Location(codenamesArray.get(i), agentArray.get(i));
-		//^^ causes an error and createAgentsTest & choose Codenames fail?
-			l[i] = loc;
+			answer.add(loc);
 		}
-		return l;
+		return answer;
 	}
 	
+	public ArrayList<String> createIlegalGuessArray(){
+		ArrayList<String> answer = new ArrayList<String>();
+		for(int i=0; i<(row*column); i++) {
+			answer.add(codenamesArray.get(i));
+		}
+		return answer;
+	}
 	
+	public boolean clueCheck(String c) {
+		for(String s : illegalGuessArray) {
+			if(s.equals(c)) {
+				return false;
+			}
+		}
+		return true;
+	}
 	
+	public boolean selected(Location l) {
+		l.revealed = true; 
+		for(int i=0; i<illegalGuessArray.size(); i++) {
+			if(l.codename.equals(illegalGuessArray.get(i))) {
+				illegalGuessArray.remove(illegalGuessArray.get(i));
+			}
+		}
+		count--;
+		if((redTurn && l.agent.equals("Red")) || (blueTurn && l.agent.equals("Blue"))){
+			return true;
+		}
+		return false;
+	}
 	
-	/**
-	 * 
-	 * @param index at which arraylist returns a string.
-	 * @return will return the string at the index in the arraylist.
-	 */
-	public String getRedArray(int i) {
-		return redArray.get(i);
+	public boolean winningState() {
+		int redCount = 0;
+		int blueCount = 0;
+		int assassinCount = 0;
+			for(Location l : board.locArray) {
+				if(l.revealed && l.agent.equals("Red")) {
+					redCount++;
+				}
+				if(l.revealed && l.agent.equals("Blue")) {
+					blueCount++;
+				}
+				if(l.revealed && l.agent.equals("Assassin")) {
+					assassinCount++;
+				}
+			}
+		if(redCount == redAgents || blueCount == blueAgents || assassinCount == assassins) {
+			return true;
+		}
+		return false;
 	}
-
-
-	/**
-	 * @return chooses a blue agent from the bluearray arraylist<string> at the indicated index
-	 */
-	public String getBlueArray(int i) {
-		return blueArray.get(i);
+	
+	public String assassinRevealed() {
+		if(redTurn) {
+			return "Blue";
+		}
+		if(blueTurn) {
+			return "Red";
+		}
+		return null;
 	}
-
-
-	/**
-	 * @return chooses a bystander from the bystanderarray arraylist<string> at the indicated index
-	 */
-	public String getBystanderArray(int i) {
-		return bystanderArray.get(i);
-	}
-
-	/**
-	 * @return chooses an assassin from the assassinarray arraylist<string> at the indicated index
-	 */
-	public String getAssassinArray(int i) {
-		return assassinArray.get(i);
-	}
-
-	/**
-	 * @param index
-	 * @return get a code name from codenamesArray at the indicated index
-	 */
-	public String getCodenamesArray(int i) {
-		return codenamesArray.get(i);
-	}
-
-	public String getAgentArray(int index) {
-		return agentArray.get(index);
 }
-	}
