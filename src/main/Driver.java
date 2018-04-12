00d55f2eedff08347cd5af0aa0eb4a81ea4238bf
jmorrow2 <36283240+jmorrow2@users.noreply.github.com>
2018-04-12 15:12:07 -0400

    Update Driver.java

--- src/main/Driver.java
+++ src/main/Driver.java
@@ -1,13 +1,15 @@
 package main;
 
+/**@author James Morrow
+@author Harsh Patel
+@author Victoria Dib
+@author Jason Zhou
+@author Kimberly So
+*/
+
 import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
-
-import com.sun.glass.events.KeyEvent;
 
 import code.Model;
 import gui.GUI;
@@ -17,48 +19,38 @@ public class Driver implements Runnable {
 	private Model _model;
 	private JFrame _window;
 	private JPanel _mainPanel;
-	private JMenuBar menuBar;
-	private JMenu menu;
-	private JMenuItem quit,restart;
 	
 	public Driver(Model m) {
 		_model = m;
 	}
 	
 	public static void main(String[] args) {
-		Model m = new Model(5,5,9,8,7,1,"src/code/GameWords.txt");
+		Model m = new Model(5,5,9,8,7,1,"src/GameWords.txt");
 		SwingUtilities.invokeLater(new Driver(m));
 	}
 
 	@Override
 	public void run() {
-		set_window(new JFrame("Codenames"));
+		_window = new JFrame("Codenames");
 		_mainPanel = new JPanel();
-		get_window().getContentPane().add(_mainPanel);
+		_window.getContentPane().add(_mainPanel);
 		
-		//file in header
-//		menuBar = new JMenuBar();
-//		menu = new JMenu("File");
-//		menu.setMnemonic(KeyEvent.VK_F);
-//		menuBar.add(menu);
-//		_window.getContentPane().add(menuBar);
-//		
-//		quit = new JMenuItem("Quit Game");
-//		restart = new JMenuItem("Restart game");
-//		
-//		menu.add(quit);
-//		menu.add(restart);
-
 		new GUI(_model, _mainPanel, this);
 		
-		get_window().setVisible(true);
-		get_window().pack();
-		get_window().setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
+		_window.setVisible(true);
+		_window.pack();
+		_window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
+	}
+	
+	public void restart() {
+		_window.dispose();
+		_model.setUp();
+		run();
 	}
 	
 	public void updateJFrame() {
-		get_window().pack();
-		get_window().repaint();
+		_window.pack();
+		_window.repaint();
 	}
 
 	/**
@@ -66,12 +58,5 @@ public class Driver implements Runnable {
 	 */
 	public JFrame get_window() {
 		return _window;
-	}
-
-	/**
-	 * @param _window the _window to set
-	 */
-	public void set_window(JFrame _window) {
-		this._window = _window;
 	}
 }
