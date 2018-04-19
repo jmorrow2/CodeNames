36bf746453ef9af7b2aca3f055cac45b85e48dce
Victoria Dib <36283261+VictoriaDib@users.noreply.github.com>
2018-04-19 01:33:07 -0400

    Update twoPlayerActionListener.java

--- src/event_handlers/twoPlayerActionListener.java
+++ src/event_handlers/twoPlayerActionListener.java
@@ -5,6 +5,10 @@ import java.awt.event.ActionEvent;
 
 
 import java.awt.event.ActionListener;
+
+import javax.swing.SwingUtilities;
+
+import code.Model;
 import main.Driver;
 
 /**
@@ -26,15 +30,18 @@ public class twoPlayerActionListener implements ActionListener {
 		d = drv;
 	}
 	
-	/**method that restarts the game
-	 * calls game to restart w/ 2 players method on the Driver*/
+	/**
+	 * method that will restart the game with two players by calling methods made in the driver.
+	 */
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		d.restart();	
-		d.run();
+		d.restart();	 
 		
+		Model m = new Model(5,5,9,8,0,7,1,"src/GameWords.txt"); //(rows, columns,red,blue, green,bystanders,assassins,file_) -- 2 Players
 
+		SwingUtilities.invokeLater(new Driver(m));
+		
 		
 	}
 }
