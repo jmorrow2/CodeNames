f4999411ba44882b58199aa450bb1d471a6b4b42
Victoria Dib <36283261+VictoriaDib@users.noreply.github.com>
2018-04-19 01:32:43 -0400

    Update threePlayerActionListener.java

--- src/event_handlers/threePlayerActionListener.java
+++ src/event_handlers/threePlayerActionListener.java
@@ -1,10 +1,8 @@
 package event_handlers;
 
-
 import java.awt.event.ActionEvent;
-
-
 import java.awt.event.ActionListener;
+
 
 import javax.swing.SwingUtilities;
 
@@ -19,29 +17,33 @@ import main.Driver;
  * @author Kimberly So
  */
 
-public class twoPlayerActionListener implements ActionListener {
+public class threePlayerActionListener implements ActionListener {
 	/**GUI connected to this action listener*/
 	private Driver d;
 	
+	private Model m;
 
+
+	
 	/**@param g - GUI
 	 * */
-	public twoPlayerActionListener(Driver drv) {
+	public threePlayerActionListener(Driver drv) {
 		d = drv;
 	}
 	
-	/**
-	 * method that will restart the game with two players by calling methods made in the driver.
-	 */
+	/**method that restarts the game
+	 * calls game to restart with 3 players method on the Driver*/
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		d.restart();	 
 		
-		Model m = new Model(5,5,9,8,0,7,1,"src/GameWords.txt"); //(rows, columns,red,blue, green,bystanders,assassins,file_) -- 2 Players
+		d.restart();
+		
+		m = new Model(5,5,6,5,5,7,2,"src/GameWords.txt"); //(rows, columns,red,blue,green,bystanders,assassins,file_) -- 3 Players
 
 		SwingUtilities.invokeLater(new Driver(m));
-		
-		
-	}
+
+
+	}		
 }
+
